cb2e5aab2f932b585466a69710b6f896cf37917a
Nathan Lin <nlin@athenian.org>
2022-02-16 09:13:38 -0800

    draft

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,27 +3,68 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculator {
-
-    public Calculator() {
+    public static void main(String[] args) {
         // 1. Create the frame (the window that will pop up)
         JFrame frame = new JFrame("Calculator");
-        // frame.setLayout(...);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
         // 2. Choose what happens when you click the exit button
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // 3. Create components and put them in the frame
 
-        // YOUR CODE HERE
+        JTextField field1 = new JTextField(15);
+
+        JPanel panel1 = new JPanel();
+        JButton button7 = new JButton("7");
+        panel1.add(button7);
+        JButton button8 = new JButton("8");
+        panel1.add(button8);
+        JButton button9 = new JButton("9");
+        panel1.add(button9);
+        JButton buttonDivide = new JButton("/");
+        panel1.add(buttonDivide);
+
+        JPanel panel2 = new JPanel();
+        JButton button4 = new JButton("4");
+        panel2.add(button4);
+        JButton button5 = new JButton("5");
+        panel2.add(button5);
+        JButton button6 = new JButton("6");
+        panel2.add(button6);
+        JButton buttonTimes = new JButton("*");
+        panel2.add(buttonTimes);
+
+        JPanel panel3 = new JPanel();
+        JButton button1 = new JButton("1");
+        panel3.add(button1);
+        JButton button2 = new JButton("2");
+        panel3.add(button2);
+        JButton button3 = new JButton("3");
+        panel3.add(button3);
+        JButton buttonMinus = new JButton("-");
+        panel3.add(buttonMinus);
+
+        JPanel panel4 = new JPanel();
+        JButton button0 = new JButton("0");
+        panel4.add(button0);
+        JButton buttonDot = new JButton(".");
+        panel4.add(buttonDot);
+        JButton buttonEquals = new JButton("=");
+        panel4.add(buttonEquals);
+        JButton buttonPlus = new JButton("+");
+        panel4.add(buttonPlus);
+
+        frame.add(field1);
+        frame.add(panel1);
+        frame.add(panel2);
+        frame.add(panel3);
+        frame.add(panel4);
 
         // 4. Size the frame
         frame.pack();
 
         // 5. Show the frame
         frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        Calculator c = new Calculator();
     }
 }
--- src/main/java/QuadForm.java
+++ src/main/java/QuadForm.java
@@ -7,14 +7,20 @@ public class QuadForm {
     public QuadForm() {
         //1. Create the frame.
         JFrame frame = new JFrame("Quadratic Formula");
-        // frame.setLayout(...);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
         //2. Optional: What happens when the frame closes?
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //3. Create components and put them in the frame.
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.PAGE_AXIS));
+        JPanel panel1 = new LTPanel("a =", 10);
+        frame.add(panel1);
+
+        JPanel panel2 = new LTPanel("b =", 10);
+        frame.add(panel2);
+
+        JPanel panel3 = new LTPanel("C =", 10);
+        frame.add(panel3);
 
         //4. Size the frame.
         // frame.setSize(400, 400);
